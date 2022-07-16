af3d228f8c64128effe99364e77d3026c56dbe40
Nataly Suvorova <suvorova.nata.a@gmail.com>
2022-07-16 17:31:58 +0300

    yes

--- 001_forLection/Program.cs
+++ 001_forLection/Program.cs
@@ -5,38 +5,31 @@ if (inNumber != null)
 {
     int number = int.Parse(inNumber);
     string[] dayOfWeek = new string[7];
-    dayOfWeek[0] = "понелельник";
-    dayOfWeek[1] = "вторник";
-    dayOfWeek[2] = "среда";
-    dayOfWeek[3] = "четверг";
-    dayOfWeek[4] = "пятница";
-    dayOfWeek[5] = "суббота";
-    dayOfWeek[6] = "воскресенье";
-    Console.WriteLine(dayOfWeek[number - 1]);
+    switch (number)
+    {
+        case 1:
+            Console.WriteLine("понедельник");
+            break;
+        case 2:
+            Console.WriteLine("вторник");
+            break;
+        case 3:
+            Console.WriteLine("среда");
+            break;
+        case 4:
+            Console.WriteLine("четверг");
+            break;
+        case 5:
+            Console.WriteLine("пятница");
+            break;
+        case 6:
+            Console.WriteLine("суббота");
+            break;
+        case 7:
+            Console.WriteLine("воскресенье");
+            break;
+    }
 }
 
 
-// switch (number)
-// {
-//     case 1:
-//         Console.WriteLine("понедельник");
-//         break;
-//     case 2:
-//         Console.WriteLine("вторник");
-//         break;
-//     case 3:
-//         Console.WriteLine("среда");
-//         break;
-//     case 4:
-//         Console.WriteLine("четверг");
-//         break;
-//     case 5:
-//         Console.WriteLine("пятница");
-//         break;
-//     case 6:
-//         Console.WriteLine("суббота");
-//         break;
-//     case 7:
-//         Console.WriteLine("воскресенье");
-//         break;
-// }
+

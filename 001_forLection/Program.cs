45051c2d4a7902497cb5e50191b661ae5b3a2152
Nataly Suvorova <suvorova.nata.a@gmail.com>
2022-07-16 17:59:48 +0300

    yes

--- 001_forLection/Program.cs
+++ 001_forLection/Program.cs
@@ -1,32 +1,16 @@
 ﻿// четвертая задача на 1-ом семинаре. Вывести целые числа от -N до N
+
 Console.WriteLine("Введите положительное число");
 string? inNumber = Console.ReadLine();
 if (inNumber != null)
 {
     int number = int.Parse(inNumber);
-    int startNumber = number * -1;
-    string line = "";
-    while (startNumber < number)
+    int startNumber = 1;
+    string line = "0";
+    while (startNumber <= number)
     {
-        line = line + startNumber + ", ";
+        line = startNumber * (-1) + ", " + line + ", " + startNumber + ", ";
         startNumber++;
     }
-    line = line + number;
-    Console.WriteLine(line);
+        Console.WriteLine(line);
 }
-
-
-// Console.WriteLine("Введите положительное число");
-// string? inNumber = Console.ReadLine();
-// if (inNumber != null)
-// {
-//     int number = int.Parse(inNumber);
-//     int startNumber = 1;
-//     string line = "0";
-//     while (startNumber <= number)
-//     {
-//         line = startNumber * (-1) + ", " + line + ", " + startNumber + ", ";
-//         startNumber++;
-//     }
-//         Console.WriteLine(line);
-// }

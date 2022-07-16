9bc823acb6104c6834b223bfdec7b975d0886830
Nataly Suvorova <suvorova.nata.a@gmail.com>
2022-07-16 18:15:58 +0300

    yes

--- 001_forLection/Program.cs
+++ 001_forLection/Program.cs
@@ -1,18 +1,9 @@
 ﻿// пятая задача. Показывает последюю цифру 3-х значного числа
 Console.WriteLine("Введите 3-х значное число");
+
 string? inNumber = Console.ReadLine();
 if (inNumber != null)
 {
-    int number = int.Parse(inNumber);
-    int outNumber = number%10;
-    Console.WriteLine(outNumber);
-
+    char [] M = inNumber.ToCharArray();
+    Console.WriteLine(M[2]);
 }
-
-
-// string? inNumber = Console.ReadLine();
-// if (inNumber != null)
-// {
-//     char [] M = inNumber.ToCharArray();
-//     Console.WriteLine(M[2]);
-// }

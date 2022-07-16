d22463bc7a858b19768c525533b642e5acda1a92
Nataly Suvorova <suvorova.nata.a@gmail.com>
2022-07-16 17:57:24 +0300

    yes

--- 001_forLection/Program.cs
+++ 001_forLection/Program.cs
@@ -1,35 +1,32 @@
-﻿// третья задача на 1-ом семинаре. Вывести день недели по номеру
-Console.WriteLine("Введите число от 1 до 7");
+﻿// четвертая задача на 1-ом семинаре. Вывести целые числа от -N до N
+Console.WriteLine("Введите положительное число");
 string? inNumber = Console.ReadLine();
 if (inNumber != null)
 {
     int number = int.Parse(inNumber);
-    string[] dayOfWeek = new string[7];
-    switch (number)
+    int startNumber = number * -1;
+    string line = "";
+    while (startNumber < number)
     {
-        case 1:
-            Console.WriteLine("понедельник");
-            break;
-        case 2:
-            Console.WriteLine("вторник");
-            break;
-        case 3:
-            Console.WriteLine("среда");
-            break;
-        case 4:
-            Console.WriteLine("четверг");
-            break;
-        case 5:
-            Console.WriteLine("пятница");
-            break;
-        case 6:
-            Console.WriteLine("суббота");
-            break;
-        case 7:
-            Console.WriteLine("воскресенье");
-            break;
+        line = line + startNumber + ", ";
+        startNumber++;
     }
+    line = line + number;
+    Console.WriteLine(line);
 }
 
 
-
+// Console.WriteLine("Введите положительное число");
+// string? inNumber = Console.ReadLine();
+// if (inNumber != null)
+// {
+//     int number = int.Parse(inNumber);
+//     int startNumber = 1;
+//     string line = "0";
+//     while (startNumber <= number)
+//     {
+//         line = startNumber * (-1) + ", " + line + ", " + startNumber + ", ";
+//         startNumber++;
+//     }
+//         Console.WriteLine(line);
+// }

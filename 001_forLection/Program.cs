57e3215c0f421779b5483bb7ccba6c75dd62fdfb
Nataly Suvorova <suvorova.nata.a@gmail.com>
2022-07-16 17:14:21 +0300

    yes

--- 001_forLection/Program.cs
+++ 001_forLection/Program.cs
@@ -1,2 +1,29 @@
-﻿// первая програ
-Console.WriteLine("привет мир");
+﻿// вторая задача на 1-ом семинаре. Яв-ля ли первое число квадратом второго.
+Console.WriteLine("Введите первое число");
+string? inNumber1 = Console.ReadLine();
+Console.WriteLine("Введите второе число");
+String? inNumber2 = Console.ReadLine();
+if (inNumber1 != null && inNumber2 != null)
+{
+    int number1 = int.Parse(inNumber1);
+    int number2 = int.Parse(inNumber2);
+    if (number1 == number2 * number2)
+    {
+        Console.Write("Yes");
+    }
+    else
+    {
+        Console.Write("No");
+    }
+
+}
+
+
+    // if (Math.Sqrt(number1) == number2)
+    // {
+    //     Console.WriteLine("Yes");
+    // }
+    // else
+    // {
+    //     Console.WriteLine("No");
+    // }

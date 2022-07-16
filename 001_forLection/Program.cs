2df61a61858cd417d09afc68d9d87b0b70880dd9
Nataly Suvorova <suvorova.nata.a@gmail.com>
2022-07-16 17:27:56 +0300

    yes

--- 001_forLection/Program.cs
+++ 001_forLection/Program.cs
@@ -1,20 +1,42 @@
-﻿// вторая задача на 1-ом семинаре. Яв-ля ли первое число квадратом второго.
-Console.WriteLine("Введите первое число");
-string? inNumber1 = Console.ReadLine();
-Console.WriteLine("Введите второе число");
-String? inNumber2 = Console.ReadLine();
-if (inNumber1 != null && inNumber2 != null)
+﻿// третья задача на 1-ом семинаре. Вывести день недели по номеру
+Console.WriteLine("Введите число от 1 до 7");
+string? inNumber = Console.ReadLine();
+if (inNumber != null)
 {
-    int number1 = int.Parse(inNumber1);
-    int number2 = int.Parse(inNumber2);
-    if (Math.Sqrt(number1) == number2)
-    {
-        Console.WriteLine("Yes");
-    }
-    else
-    {
-        Console.WriteLine("No");
-    }
+    int number = int.Parse(inNumber);
+    string[] dayOfWeek = new string[7];
+    dayOfWeek[0] = "понелельник";
+    dayOfWeek[1] = "вторник";
+    dayOfWeek[2] = "среда";
+    dayOfWeek[3] = "четверг";
+    dayOfWeek[4] = "пятница";
+    dayOfWeek[5] = "суббота";
+    dayOfWeek[6] = "воскресенье";
+    Console.WriteLine(dayOfWeek[number - 1]);
 }
 
 
+// switch (number)
+// {
+//     case 1:
+//         Console.WriteLine("понедельник");
+//         break;
+//     case 2:
+//         Console.WriteLine("вторник");
+//         break;
+//     case 3:
+//         Console.WriteLine("среда");
+//         break;
+//     case 4:
+//         Console.WriteLine("четверг");
+//         break;
+//     case 5:
+//         Console.WriteLine("пятница");
+//         break;
+//     case 6:
+//         Console.WriteLine("суббота");
+//         break;
+//     case 7:
+//         Console.WriteLine("воскресенье");
+//         break;
+// }
